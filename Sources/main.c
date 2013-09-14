125de66970e82bbb504e6b0afbb3ef53954d6e04
kotofos <ibelkov@gmail.com>
2013-09-14 09:47:14 +0700

    gpio init added

--- Sources/main.c
+++ Sources/main.c
@@ -1,7 +1,32 @@
+#include "stm32f10x.h"
+
+#define ZON GPIO_SetBits(GPIOB, GPIO_Pin_0)
+#define ZOFF GPIO_ResetBits(GPIOB, GPIO_Pin_0)
+
+void test(void) {
+    return;
+}
 
 int main()
 {
+    SystemInit();
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    //data
+    GPIO_StructInit(&GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init (GPIOA, &GPIO_InitStructure);
     
-    
+    //power
+    GPIO_StructInit(&GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init (GPIOB, &GPIO_InitStructure);
+    ZON;
+    test();
+    ZOFF;
     
 }
