e9ba428baff89a6180b7b57b37eb168638560af3
kotofos <ibelkov@gmail.com>
2013-09-14 11:09:01 +0700

    live watch

--- Sources/main.c
+++ Sources/main.c
@@ -12,22 +12,22 @@ uint32_t bitTim[50], bitTimCount = 1;
 int main()
 {
     SystemInit();
-//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
-//    GPIO_InitTypeDef GPIO_InitStructure;
-//    //data
-//    GPIO_StructInit(&GPIO_InitStructure);
-//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
-//    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-//    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-//    GPIO_Init (GPIOA, &GPIO_InitStructure);
-//    
-//    //power
-//    GPIO_StructInit(&GPIO_InitStructure);
-//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-//    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-//    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-//    GPIO_Init (GPIOB, &GPIO_InitStructure);
-//    
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
+    GPIO_InitTypeDef GPIO_InitStructure;
+    //data
+    GPIO_StructInit(&GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init (GPIOA, &GPIO_InitStructure);
+    
+    //power
+    GPIO_StructInit(&GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init (GPIOB, &GPIO_InitStructure);
+    
     //Tim1 for input capture
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
     
@@ -40,15 +40,16 @@ int main()
     TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
     TIM_TimeBaseInit(TIM1, &TIM_TimeBaseInitStructure);
     
-//    TIM_ICInitTypeDef TIM_ICInitStructure;
-//    TIM_ICStructInit(&TIM_ICInitStructure);
-//    TIM_ICInitStructure.TIM_Channel = TIM_Channel_4;
-//    TIM_ICInitStructure.TIM_ICFilter = 0x0;
-//    TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Falling;
-//    TIM_ICInitStructure.TIM_ICPrescaler = 0x0;
-//    TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI; 
-//    TIM_ICInit(TIM1, &TIM_ICInitStructure);
-//    TIM_ITConfig(TIM1, TIM_EventSource_CC4, ENABLE);
+    TIM_ICInitTypeDef TIM_ICInitStructure;
+    TIM_ICStructInit(&TIM_ICInitStructure);
+    TIM_ICInitStructure.TIM_Channel = TIM_Channel_4;
+    TIM_ICInitStructure.TIM_ICFilter = 0x0;
+    TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Falling;
+    TIM_ICInitStructure.TIM_ICPrescaler = 0x0;
+    TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI; 
+    TIM_ICInit(TIM1, &TIM_ICInitStructure);
+    TIM_ITConfig(TIM1, TIM_EventSource_CC4, ENABLE);
+    NVIC_EnableIRQ(TIM1_CC_IRQn);
     
     //tim3 for delay
     RCC_APB1PeriphClockCmd((RCC_APB1Periph_TIM3), ENABLE);
@@ -60,16 +61,13 @@ int main()
     TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
     TIM_TimeBaseInitStructure.TIM_RepetitionCounter = 0;
     TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure);
-    
     TIM_Cmd(TIM3, ENABLE);
     
     
-    NVIC_EnableIRQ(TIM1_CC_IRQn);
-       
-    ZON;
-    delayMs(1000, TIM3);
-    TIM_Cmd(TIM1, ENABLE);
     
+    ZON;
+    delayMs(3, TIM3);
+    TIM_Cmd(TIM1, ENABLE);
     while (bitTimCount < 31) {};
     ZOFF;
     
@@ -78,7 +76,7 @@ int main()
 void TIM1_CC_IRQHandler(void) 
 { 
     int a;
-   bitTim[bitTimCount] = (a = TIM_GetCapture4(TIM1)) - bitTim[bitTimCount - 1];
+    bitTim[bitTimCount] = (a = TIM_GetCapture4(TIM1)) - bitTim[bitTimCount - 1];
     ++bitTimCount;
 }
 
